417beaadd4b08c2eb621686ef2aea38cde86fd53
784757555@qq.com <784757555@qq.com>
2017-05-17 21:33:28 +0800

    侧边栏选择

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -2,23 +2,11 @@
 	<div id="resumeEditor">
 		<nav>
 			<ol>
-				<li class="active">操作</li>
-				<li>操作</li>
-				<li>操作</li>
-				<li>操作</li>
-				<li>操作</li>
-				<li>操作</li>
-				<li>操作</li>
+				<li v-for="(item,index) in resume.visibleItems" :class="{active: item === selected}" @click="selected = item">{{index}}</li>
 			</ol>
 		</nav>
 		<ol class="panels">
-			<li>1</li>
-			<li>2</li>
-			<li>3</li>
-			<li>4</li>
-			<li>5</li>
-			<li>6</li>
-			<li>7</li>
+			<li v-for='item in resume.visibleItems' v-show="item===selected">{{resume[item]}}</li>
 		</ol>	
 	</div>
 </template>
@@ -28,6 +16,21 @@ export default{
 	name: "ResumeEditor",
 	data() {
 		return {
+			selected:'bio',
+			resume:{
+				visibleItems:['bio','work history','education','projects','awards','contacts','other'],
+				bio:{
+					name:'',
+					city:'',
+					title:''
+				},
+				'work history':[],
+				education: [],
+				projects: [],
+				awards: [],
+				contacts: [],
+				other:[]
+			}
 
 		}
 	}
@@ -40,12 +43,14 @@ export default{
 		box-shadow:0 1px 3px 0 rgba(0,0,0,.25);
 		display: flex;
 		flex-direction: row;
+		overflow: auto;
 		> nav{
 			width: 80px;
 			background-color: black;
 			color: white;
 			> ol{
 				>li{
+					cursor: pointer;
 					height: 48px;
 					display: flex;
 					justify-content: center;
