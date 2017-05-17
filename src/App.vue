688477310f71eff013da1d5de5ecfa2eb14f257a
784757555@qq.com <784757555@qq.com>
2017-05-17 16:46:00 +0800

    second commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{text}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function(){
+    return {
+      text: 'Hello world'
+    }
+  }
+}
+</script>
+
+<style>
+  p{color:red}
+</style>
