7a4cb9f26ac66da5fca8c46a2b54dda5aae364f4
784757555@qq.com <784757555@qq.com>
2017-05-18 13:45:13 +0800

    将原有的数据移动到store里

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -32,49 +32,15 @@
 <script>
 export default{
 	name: "ResumeEditor",
-	data() {
-		return {
-			selected:'profile',
-			resume:{
-				config:[
-				{field:'profile',icon:'id'},
-				{field:'work history',icon:'work'},
-				{field:'education',icon:'book'},
-				{field:'projects',icon:'heart'},
-				{field:'awards',icon:'cup'},
-				{field:'contacts',icon:'phone'},
-				],
-				profile:{
-					name:'李子豪',
-					city:'中国',
-					title:'简'
-				},
-				'work history':[
-				{company:'AL',content:'我的第一个工作'},
-				{company:'TX',content:'我的第一个工作'}
-				],
-				education: [
-				{school:'AL',content:'我的大学'},
-				{school:'TX',content:'我的大学'}
-				],
-				projects: [
-				{name:'AL',content:'我的大学'},
-				{name:'TX',content:'我的大学'}
-				],
-				awards: [
-				{name:'AL',content:'我的大学'},
-				{name:'TX',content:'我的大学'}
-				],
-				contacts: [
-				{contact:'AL',content:'我的大学'},
-				{contact:'TX',content:'我的大学'}
-				],
-			}
-		}
-	},
 	computed: {
 		count (){
 			return this.$store.state.count
+		},
+		selected (){
+			return this.$store.state.selected
+		},
+		resume (){
+			return this.$store.state.resume
 		}
 	},
 	methods: {
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,44 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
 	state:{
-		count: 0
-	},
+		count:0,
+		selected:'profile',
+			resume:{
+				config:[
+				{field:'profile',icon:'id'},
+				{field:'work history',icon:'work'},
+				{field:'education',icon:'book'},
+				{field:'projects',icon:'heart'},
+				{field:'awards',icon:'cup'},
+				{field:'contacts',icon:'phone'},
+				],
+				profile:{
+					name:'李子豪',
+					city:'中国',
+					title:'简'
+				},
+				'work history':[
+				{company:'AL',content:'我的第一个工作'},
+				{company:'TX',content:'我的第一个工作'}
+				],
+				education: [
+				{school:'AL',content:'我的大学'},
+				{school:'TX',content:'我的大学'}
+				],
+				projects: [
+				{name:'AL',content:'我的大学'},
+				{name:'TX',content:'我的大学'}
+				],
+				awards: [
+				{name:'AL',content:'我的大学'},
+				{name:'TX',content:'我的大学'}
+				],
+				contacts: [
+				{contact:'AL',content:'我的大学'},
+				{contact:'TX',content:'我的大学'}
+				],
+			}
+		},
 	mutations: {
 		increment (state){
 			state.count ++
