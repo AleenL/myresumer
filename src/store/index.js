2d9d78057ffcd7a3a5ec08d56180de38b9d2436b
784757555@qq.com <784757555@qq.com>
2017-05-18 14:13:40 +0800

    完善预览页面的样式

--- src/store/index.js
+++ src/store/index.js
@@ -9,23 +9,24 @@ export default new Vuex.Store({
 			resume:{
 				config:[
 				{field:'profile',icon:'id'},
-				{field:'work history',icon:'work'},
+				{field:'workHistory',icon:'work'},
 				{field:'education',icon:'book'},
 				{field:'projects',icon:'heart'},
 				{field:'awards',icon:'cup'},
 				{field:'contacts',icon:'phone'},
 				],
 				profile:{
-					name:'李子豪',
-					city:'中国',
-					title:'简'
+					name:'李xx',
+					city:'重庆',
+					title:'前端工程师',
+					birthday:'1990-01-01'
 				},
-				'work history':[
-				{company:'AL',content:'我的第一个工作'},
-				{company:'TX',content:'我的第一个工作'}
+				'workHistory':[
+				{company:'广达电脑制造公司',content:'我的第一个工作，没什么技术含量，就是给别人打下手'},
+				{company:'肯德基快餐店',content:'后厨帮忙，一般就是在厨房炸薯条啦，炸鸡腿了，做汉堡的肉啦'}
 				],
 				education: [
-				{school:'AL',content:'我的大学'},
+				{school:'为难师范学院',content:'本科学历'},
 				{school:'TX',content:'我的大学'}
 				],
 				projects: [
