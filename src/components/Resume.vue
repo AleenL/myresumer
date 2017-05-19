7107c548b438de96976cbd3e8ac3f70df4a599e3
784757555@qq.com <784757555@qq.com>
2017-05-19 12:43:10 +0800

    添加预览功能

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -13,16 +13,24 @@
 </template>
 
 <script>
-  import Topbar from './Topbar'
-  import ResumeEditor from './ResumeEditor'
-  import ResumePreview from './ResumePreview'
-  export default {
-    name: 'Resume',
-    components: { Topbar, ResumeEditor, ResumePreview},
-    created() {
-    }
+import Topbar from './Topbar'
+import ResumeEditor from './ResumeEditor'
+import ResumePreview from './ResumePreview'
+export default {
+  name: 'Resume',
+  components: { Topbar, ResumeEditor, ResumePreview},
+  created() {
   }
+}
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+#resumeEditor{
+  min-width:35%;
+  background:#444;
+}
+#resumePreview{
+  flex-grow:1;
+  margin-left:16px;
+}
 </style>
